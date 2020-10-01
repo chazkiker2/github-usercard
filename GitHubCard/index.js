48c149978a2ff9559b2eff100f4d9a17ba23ab13
chazkiker2 <chaz-kiker@lambdastudents.com>
2020-10-01 15:56:11 -0400

    finish index.js entirely

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,17 +1,110 @@
+// @ts-check
+"use strict";
+
 import axios from "axios";
-// console.log(axios);
-/* Edit
+
+//* STEP 1 (initial)
+// const userRequest = axios.get("https://api.github.com/users/chazkiker2")
+//   .then(res=>{
+//     console.log(`Response: ${res}`);
+//     const chazCard = createCard(res.data);
+//     const entry = document.querySelector("div.cards");
+//     entry.appendChild(chazCard);
+//   })
+//   .catch(e=>{
+//     console.log(e);
+//   });
+
+
+// >> WRAP axios get request in this insertCard function
+/**
+ * Receives a username in string format, makes axios get request to GitHub API, 
+ * creates a user card via createCard(), and appends the component HTML to the DOM
+ * @param {string} username 
+ */
+const insertCard = (username) => {
+  const endpoint = `https://api.github.com/users/${username}`;
+  axios.get(endpoint) //* step 1 & 2
+    .then( res => {
+      const card = createCard(res.data); //* step 4
+      document.querySelector("div.cards").appendChild(card); //* step 5
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  return;
+};
+
+
+//*step 5
+const followersArray = ["reesharper", "chazkiker2", "sekotszs", "bigknell", "tetondan"];
+followersArray.forEach( usr => insertCard(usr));
+
+
+//>> STEP 3
+/**
+ * This method takes an Object of data from the axios GET request.
+ * From the data provided, createCard builds a user component and returns the component's HTML
+ * @param {Object} user 
+ * @return HTML Element 
+ */
+const createCard = (user) => { 
+  const cardDiv = document.createElement("div"); //class="card"
+    const profileImg = document.createElement("img"); //src=`${user.avatar_url}`
+    const infoDiv = document.createElement("div"); //class="card-info"
+      const nameH3 = document.createElement("h3"); //class="name", textContent=user.name
+      const usernameP = document.createElement("p"); //class="username", textContent=user.login
+      const locationP = document.createElement("p"); //textContent=`Location: ${user.location}`
+        const linkToGithub = document.createElement("a"); //href=`${user.url}`
+      const followersP = document.createElement("p"); //textContent=`Followers: ${user.followers}
+      const followingP = document.createElement("p"); //textContent=`Following: ${user.following}`
+      const bioP = document.createElement("p"); //textContent=`Bio: ${user.bio}`
+  
+  cardDiv.append(profileImg, infoDiv);
+  infoDiv.append(nameH3, usernameP, locationP, followersP, followingP, bioP);
+  locationP.append(linkToGithub);
+
+  cardDiv.classList.add("card");
+  profileImg.setAttribute("src", `${user.avatar_url}`);
+  infoDiv.classList.add("card-info");
+  nameH3.classList.add("name");
+  nameH3.textContent = `${user.name}`;
+  usernameP.classList.add("username");
+  usernameP.textContent = `${user.login}`;
+  locationP.textContent = `Location: ${user.location}`;
+  linkToGithub.setAttribute("href", `${user.url}`);
+  followersP.textContent = `Followers: ${user.followers}`;
+  followingP.textContent = `Following: ${user.following}`;
+  bioP.textContent = `Bio: ${user.bio}`;
+
+  return cardDiv;
+};
+
+/*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-const userRequest = axios.get("https://api.github.com/users/chazkiker2")
-  .then(res=>{
-    console.log(res);
-  })
-  .catch(e=>{
-    console.log(e);
-  });
+
+/*
+  STEP 3: Create a function that accepts a single object as its only argument.
+    Using DOM methods and properties, create and return the following markup:
+
+    <div class="card">
+      <img src={image url of user} />
+      <div class="card-info">
+        <h3 class="name">{users name}</h3>
+        <p class="username">{users user name}</p>
+        <p>Location: {users location}</p>
+        <p>Profile:
+          <a href={address to users github page}>{address to users github page}</a>
+        </p>
+        <p>Followers: {users followers count}</p>
+        <p>Following: {users following count}</p>
+        <p>Bio: {users bio}</p>
+      </div>
+    </div>
+*/
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -35,28 +128,6 @@ const userRequest = axios.get("https://api.github.com/users/chazkiker2")
 
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
-
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
-
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
 */
 
 /*
